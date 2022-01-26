db52dea0827d417a22b0f494251bf49657009f9e
Eliah7 <eliahmbwilo7@gmail.com>
2022-01-26 19:51:53 +0300

    matmul paraller

--- hello2.c
+++ hello2.c
@@ -3,12 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-#pragma omp parallel
+    omp_set_dynamic(0);
+    omp_set_num_threads(4);
+    printf("%d thread limit \n", omp_get_thread_limit());
+    printf("%d thread count \n", omp_get_num_threads());
+#pragma omp parallel num_threads(4)
     {
-        omp_set_num_threads(4);
+
         int threadNo = omp_get_thread_num();
         printf("Hello world from %d \n", threadNo);
-        printf("There are %d threads", omp_get_num_threads());
+        printf("There are %d threads\n", omp_get_num_threads());
     }
     return 0;
 }
